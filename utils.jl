b70bbd0dceb96a2bf5a64f3c3e9143087d0e96ee
Tim Besard <tim.besard@gmail.com>
2024-01-02 17:05:10 +0100

    Don't actively redirect users.

--- utils.jl
+++ utils.jl
@@ -29,23 +29,9 @@ end
 
 function hfun_redirect(params)
     url = params[1]
-
-    # XXX: is this safe?
-    #Franklin.set_var!(Franklin.LOCAL_VARS, "fd_full_url", url)
-
-    return """
-        <meta http-equiv="refresh" content="0;url=$url">
-
-        <!-- fallback 1: use Javscript, in case the browser doesn't like meta tags in the body -->
-        <script>
-            window.onload = function() {
-                window.location.href = "$url";
-            }
-        </script>
-
-        <!-- fallback 2: provide a visual element, in case Javascript is disabled -->
-        <p>This blog post is located at <a href="$url">$url</a></p>
-        """
+    # NOTE: we don't do a <meta> or <script> redirect, because that would
+    #       end up in the RSS feed which some sites pick up verbatim.
+    return """<p>This post is located at <a href="$url">$url</a></p>"""
 end
 
 function hfun_post_date()
